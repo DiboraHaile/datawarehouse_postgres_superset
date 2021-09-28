36659443b94204d964e959d0714bccbbed21cc9f
DiboraHaile <deborahaile3@gmail.com>
2021-09-28 23:35:06 +0300

    create tables using schema from mysql

--- migrate_to_postgres.py
+++ migrate_to_postgres.py
@@ -2,27 +2,42 @@ import sqlalchemy
 from sqlalchemy import create_engine
 import psycopg2
 import pandas as pd
-from mysql_query import fetch_from_mysql
-# import streamlit as st
+from fetch_from_mysql import fetch_from_mysql
+
 
 class insert_postgres:
-    def __init__(self,db,table,fetched_df):
+    """ Accept the fetched data and insert to postgres"""
+    def __init__(self,db,table):
         self.db = db
         self.table = table
-        self.fetched_df = fetched_df
+
+        
     
     def create_connection(self):
         p_engine = create_engine(f"postgresql://dibora:adminadmin@localhost:5432/{self.db}")
-        # p_engine.execute("CREATE TABLE IF NOT EXISTS records (name text PRIMARY KEY, details text[])")
         return p_engine
 
-    def write_record(self):
+
+    def create_tables(self):
+        p_engine = self.create_connection()
+        f= fetch_from_mysql('example')
+        tables = f.return_tables()
+        metadata = f.get_schema()
+        for i in range(len(tables)):
+            meta = " ".join(list(metadata[0][i].values())[:2])
+            if meta[3] == "PRI":
+                meta += " PRIMARY KEY"
+            create_query = f"CREATE TABLE IF NOT EXISTS {tables[i][0]} ({meta})"
+            p_engine.execute(create_query)
+
+
+    def write_record(self,fetched_df):
         p_engine = self.create_connection()
         columns_list = self.fetched_df.columns.tolist()
         colmns_name = ",".join(columns_list)
         no_values = ",".join(["%s" for i in range(len(columns_list))])
 
-        query = f"INSERT INTO {self.table} ({colmns_name}) VALUES ({no_values})"
+        query = f"INSERT INTO {self.db}.{self.table} ({colmns_name}) VALUES ({no_values})"
         list_rows = []
         for _,row in self.fetched_df.iterrows():
             list_rows.append(tuple([row[i] for i in range(len(columns_list))]))
@@ -34,8 +49,9 @@ class insert_postgres:
 
 
 
-# if __name__ == "__main__":
-#     df = fetch_from_mysql('example').fetch_data('records_new')
-#     print(df.head())
-#     ip = insert_postgres("example","data_new",df)
-#     ip.write_record()
+if __name__ == "__main__":
+    f= fetch_from_mysql('example')
+    ip = insert_postgres("example","data_new")
+    ip.create_tables()
+    print(ip.write_record)
+    # ip.write_record()
